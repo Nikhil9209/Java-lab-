fa1eff876a7838b5737b4a58d01d553f6f27f76d
Nikhil Jangid <nikhiljangid1704@gmail.com>
2026-08-19 09:31:14 +0530

    second commit

---
+++ Lab1/main.java
@@ -0,0 +1,14 @@
+public class main{
+
+        public static void main(String[] args){
+            cmodel model = new cmodel();
+            cviews view = new cviews();
+           new ccontroller(model , view);
+          
+
+            System.out.println("calculator");
+        }
+
+
+
+}   
